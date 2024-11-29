9bb7121e423156d2da7a0988d031cbc57fdc5cc2
sawaidbasit <sawaidbasit221@gmail.com>
2024-11-29 20:24:38 +0500

    ascending to descending low to high functionality

--- controllers/products.js
+++ controllers/products.js
@@ -2,15 +2,27 @@ const Products = require('../models/product')
 
 
 const getAllProducts = async (req, res) => {
-    const {company, name, featured} = req.query;
+    const {company, name, featured, sort, select} = req.query;
     const queryObject = {};
 
     if (company) {
         queryObject.company = company;
     }
 
+    let apiData = Products.find(queryObject);
+
     if (name) {
         queryObject.name = { $regex: name, $options: "i" };
+    }
+
+    if (sort) {
+        let sortFix = sort.replace(",", " ");
+        apiData = apiData.sort(sortFix);
+    }
+
+    if (select) {
+        let selectFix = select.split(",").join(" ");
+        apiData = apiData.select(selectFix);
     }
 
     if (featured) {
@@ -18,12 +30,12 @@ const getAllProducts = async (req, res) => {
     }
     console.log(queryObject);
 
-    const myData = await Products.find(queryObject)
+    const myData = await apiData;
     res.status(200).json({myData})
 }
 
 const getAllProductsTesting = async (req, res) => {
-    const myData = await Products.find()
+    const myData = await Products.find().select("name")
     res.status(200).json({myData})
 }
 
